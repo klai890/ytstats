8b9bd2c4afa1df5dcd99c9668da2638b5718d378
Karena Lai <65944717+klai890@users.noreply.github.com>
2024-02-18 14:45:21 -0500

    Add nextjs project, add JSON generating code

---
+++ nextjsproj/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    	{/* List of months to show  */}	
+
+    	{/* For the month selected....  */}
+	    {/* Show (a) list of videos, (b)  */}
+
+      {/* (a) show list of videos */}
+
+
+      
+    </main>
+  );
+}
